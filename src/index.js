5ace4367447b019fd202810d6be6904119ab2285
Manjeet Kumar <themanjeetkr@gmail.com>
2026-04-10 22:32:15 +0530

    add src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import express from 'express';
+import cors from 'cors';
 // const app = express();
 dotenv.config()
+// basic configuration
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static("public"));
+app.use(cors({
+    origin:process.env.CORS_ORIGIN?.split(',') || "'http://localhost:5173'",
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE', 'PATCH'],
+    allowedHeaders:['Content-Type', 'Authorization']
+
+}))
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 })
---
+++ src/utils/api-response.js
@@ -0,0 +1,9 @@
+class Apiresponse{
+    constructor(statusCode, data, message="success"){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode <400;
+    }   
+}
+export {Apiresponse};
