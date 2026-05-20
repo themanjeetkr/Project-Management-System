bd9e7fc53c81359129dfdfbf16ccb0025f3eac69
Manjeet Kumar <themanjeetkr@gmail.com>
2026-05-20 13:05:20 +0530

    add the temp token

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,85 +1,138 @@
 import mongoose from "mongoose";
-import brypt from "bcrypt";
-const userSchema = new mongoose.Schema({
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
     avatar: {
-        url: {
-            type: String,
-            default: "https://placehold.co/600x400",
-        },
-        localPath: {
-            type: String,
-        },
+      url: {
+        type: String,
+        default: "https://placehold.co/600x400",
+      },
+      localPath: {
+        type: String,
+      },
     },
 
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
 
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
 
     fullname: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
 
     password: {
-        type: String,
-        required: [true, "Password is required"],
-        minlength: 8,
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
     },
 
     isEmailVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
 
     forgetPasswordToken: {
-        type: String,
+      type: String,
     },
 
     forgetPasswordExpires: {
-        type: Date,
+      type: Date,
     },
 
     emailVerificationToken: {
-        type: String,
+      type: String,
     },
 
     emailVerificationExpires: {
-        type: Date,
+      type: Date,
     },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+// PRE SAVE HOOK
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-}
-    ,
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+// COMPARE PASSWORD
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// ACCESS TOKEN
+userSchema.methods.generateAuthToken = function () {
+  const accessToken = jwt.sign(
     {
-        timestamps: true,
+      id: this._id,
+      username: this.username,
+      email: this.email,
     },
-
-    userSchema.pre("save", async function (next) {
-        if(!this.isModified("password")) {
-            return next();
-        }
-        this.password = await brypt.hash(this.password, 10);
-        next();
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
     }
+  );
 
-    ));
-    userSchema.methods.comparePassword = async function (password) {
-        return await brypt.compare(password, this.password);
+  return accessToken;
+};
+
+// REFRESH TOKEN
+userSchema.methods.generateRefreshToken = function () {
+  const refreshToken = jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
     }
-    
+  );
 
+  return refreshToken;
+};
+
+// TEMP TOKEN
+userSchema.methods.generateTemporaryToken = function () {
+  const unhashedToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unhashedToken)
+    .digest("hex");
+
+  const tokenExpiration = Date.now() + 10 * 60 * 1000;
+
+  return {
+    unhashedToken,
+    hashedToken,
+    tokenExpiration,
+  };
+};
 
 export const User = mongoose.model("User", userSchema);
